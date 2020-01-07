79a546b5f9929260c01809663f000c2f44831f9a
刘琛 <283502808@qq.com>
2020-01-07 17:19:53 +0800

    deleted:    Blacklisted_imports.go 	new file:   blacklistedImports.go

---
+++ blacklistedImports.go
@@ -0,0 +1,36 @@
+package blacklistedImports
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type Chaincode struct {
+}
+
+func (c *Chaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success([]byte("Init Success!"))
+}
+
+func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	cByte, err := json.Marshal(time.Now())
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState("key", cByte)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success([]byte("Invoke Success!"))
+}
+
+func main() {
+	if err := shim.Start(new(Chaincode)); err != nil {
+		fmt.Printf("Start Chaincode Failed, error: %s", err)
+	}
+}
